403d5eda330de3893905b1c359b028f34deef29f
marwitz93j <37061272+marwitz93j@users.noreply.github.com>
2019-09-09 13:07:02 -0500

    Create DemoServlet.java

---
+++ DemoServlet.java
@@ -0,0 +1,13 @@
+
+import java.io.IOException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class DemoServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws IOException {
+    resp.setContentType("text/plain");
+    resp.getWriter().println("{ \"name\": \"World\" }");
+  }
+}
